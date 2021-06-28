a489218ea59a42ef41d3eab8c5172fbc94e912e2
AbdelAziz AROUAOUI <aarouaoui@leparisien.fr>
2021-06-28 12:01:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import Firebase from './component/Firebase';
-import AppSync from './component/AppSync'
-import Sns from './component/Sns'
-import Websocket from './component/Websocket'
+import Form from './component/Form';
 
 
 function App() {
   return (
     <div class="row">
       <div class="col">
-        <h1>Firebase</h1>
-        <Firebase /> 
-      </div>
-      <div class="col">
-      <h1>Websocket</h1>
-        <Websocket />                              
-      </div>
-      <div class="w-100"></div>
-      <div class="col">
-      <h1>AppSync</h1>
-        <AppSync />                              
-      </div>
-      <div class="col">
-          <h1>SNS</h1>
-          <Sns />                             
+        <h1>Bo Live</h1>
+        <Form /> 
       </div>
     </div>
   )
---
+++ src/component/Form.jsx
@@ -0,0 +1,62 @@
+import React , {Component} from "react";
+import axios from 'axios';
+
+class LiveForm extends Component {
+
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          content: ""
+        };
+      }
+     
+      handleSubmitForm(event) {
+        event.preventDefault()
+        const url= "https://4ymem8ew75.execute-api.us-east-2.amazonaws.com/prod/card";
+        
+        var requestOptions = {
+            method: 'POST',
+            headers: { 
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin' : '*',
+              'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+           },
+            body: {name : "card10" , content:"ididid", id : "idididi", channel_id:"channel1"},
+        };
+        axios.post(url, requestOptions)
+        .then(res => {
+            console.log('succes');
+        }).catch(error => {
+            console.log(error);
+        })
+
+
+      }
+     
+      handleChange(event) {
+        var value = event.target.value;
+     
+        this.setState({
+          content: value
+        });
+      }
+     
+      render() {
+        return (
+          <form onSubmit={event => this.handleSubmitForm(event)}>
+            <label>
+              Content:
+              <textarea
+                type="tex"
+                value={this.state.content}
+                onChange={event => this.handleChange(event)}
+              />
+            </label>
+            <input type="submit" value="Submit" />        
+          </form>
+        );
+      }
+}
+
+export default LiveForm;
