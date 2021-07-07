c7fce7a2f5e778008049bb1d92d59451d6f9653d
aarouaoui <79322975+aarouaoui@users.noreply.github.com>
2021-07-07 09:38:37 +0200

    Update Form.jsx

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -40,7 +40,7 @@ class LiveForm extends Component {
             console.log(card.id);
             var data = JSON.stringify({
               "action": "message",
-              "message": JSON.stringify(card)
+              "message": card.content
               });
             this.client.send(data)
         }).catch(error => {
